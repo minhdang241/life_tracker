8cf251aae1ef3d6454bdc860987129d070240f44
DANG BA MINH <d.baminh@gmail.com>
2024-07-02 18:39:12 +0700

    feat: add 1st view

--- life_tracker/settings.py
+++ life_tracker/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-___f6f8wds_&+fcl%ci8!@yl7jetvnw!q(ka*e)v-txypr#w_@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lifetracker-34d81aa82cfe.herokuapp.com'
+]
 
 
 # Application definition
@@ -117,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
