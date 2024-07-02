a81b317e9ce96c76bbe1bba9a4d0d6c664ceb548
DANG BA MINH <d.baminh@gmail.com>
2024-07-02 18:39:12 +0700

    feat: add 1st view

--- tracker/views.py
+++ tracker/views.py
@@ -1,9 +1,19 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    quotes = [
+        "“You may delay, but time will not.” – Benjamin Franklin",
+        "“Don’t wait. The time will never be just right.” – Napoleon Hill",
+        "“Time is what we want most, but what we use worst.” – William Penn",
+        # Add more quotes as needed
+    ]
+    quote = random.choice(quotes)
+    return render(request, "home.html", {"quote": quote})
