0d5f3e2238e3b8b1661e17267d47800b9db39837
DANG BA MINH <d.baminh@gmail.com>
2024-07-21 19:18:32 +0700

    chore: remove redundant imports

--- tracker/views.py
+++ tracker/views.py
@@ -2,10 +2,8 @@ import random
 
 from django.shortcuts import render
 
-# Create your views here.
 
-from django.http import HttpResponse
-from django.shortcuts import render
+# Create your views here.
 
 
 def index(request):
