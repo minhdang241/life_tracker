7391468c15d33d3298dd6de1f52e50ed6a46d887
DANG BA MINH <d.baminh@gmail.com>
2025-08-09 09:52:10 +1000

    feat: setup tailwind + shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import {defineConfig} from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '::',
+    port: 8080
+  },  // bind the development server to run on port 8080
   plugins: [react()],
+  resolve: {
+    alias: {'@': path.resolve(__dirname, './src')}
+  }  // create a shortcut to import file using @/
 })
